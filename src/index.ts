539d1b2c11b5168aff111d953202e29f4183b001
A-S620 <aatikaseedat00@gmail.com>
2022-04-18 16:48:07 +0100

    created new-files path in server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import { getAllObjectsInFolder } from './aws-functions/s3-functions';
 
 dotenv.config();
 
@@ -14,6 +15,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/hello', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+app.get('/new-files', (req: Request, res: Response) => {
+  const newFiles = getAllObjectsInFolder('new-files/');
+  res.send(newFiles);
 });
 
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
