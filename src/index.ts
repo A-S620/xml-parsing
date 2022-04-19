ddfd3ce1371b9961414aa167165b18aa5f62ae21
A-S620 <aatikaseedat00@gmail.com>
2022-04-19 13:33:05 +0100

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
-import { getAllObjectsInFolder } from './aws-functions/s3-functions';
-import { Config } from './config';
 
 dotenv.config();
 
@@ -17,11 +15,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/hello', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
-app.get('/new-files', (req: Request, res: Response) => {
-  const newFiles = getAllObjectsInFolder('prod/new-files/');
-  res.send(Config.bucketName);
-});
-
 app.listen(PORT, () => console.log(`Running on ${PORT} ⚡`));
 
 export default app;
